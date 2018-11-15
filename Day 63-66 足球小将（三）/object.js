a4feb89e2e3c97758a87d746797984d187bb2db9
Jano <mongkii@hotmail.com>
2018-11-15 10:28:11 +0800

    Simplify structure

---
+++ Day 63-66 足球小将（三）/object.js
@@ -0,0 +1,124 @@
+class Player {
+    constructor(vnum, burst, stamina, skill, power) {
+        this.radius = 1;
+        this.x = 0;
+        this.y = 0;
+        this.angle = 0; // 球员面向的角度
+        this.v = 0; // 球员当前速度
+        this.current_dur = 0; // 球员当前处于最高速的时间
+        this.timer = null; // 为了可以随时停止运动，将计时器设为对象本身属性
+        this.vnum = vnum;
+        this.burst = burst;
+        this.stamina = stamina;
+        this.skill = skill;
+        this.power = power;
+    }
+
+    run(x, y, ground, interval = 10) { // interval：球员位置刷新间隔，默认为 10ms
+        var vmax = 3 + 9 * (this.vnum - 1) / 98, // 球员最高速度
+            acc_time = 4 - 3 * (this.burst - 1) / 98, // 球员加速至最高速所需时间
+            duration = 10 + 5 * (this.stamina - 1) / 98; // 球员最高速持续时间
+
+        if (this.v === 0) {
+            this.current_dur = 0; // 当速度为 0 时重置当前处于最高速时间，以便于 timer 不断开启停止时能保持速度，同时兼具还原体力功能
+        }
+
+        var width = x - this.x,
+            height = y - this.y;
+        this.angle = Math.atan(height / width);
+        if (width < 0) { // atan 范围为 (-PI/2)~(PI/2)，因此要对角度修正，使角度范围是 0~2PI
+            this.angle += Math.PI;
+        } else if (this.angle < 0) {
+            this.angle += 2 * Math.PI;
+        }
+        var place_now = Math.sqrt(width * width + height * height), // 球员与终点的上次与这次的距离差，用于判断是否到达终点
+            place_last = place_now + 1;
+
+        var unit_time = 1000 / interval; // 单位时间
+        this.timer = setInterval(() => {
+            if (place_last - place_now > 0) { // 未到达终点前（即距离差在不断缩小）
+                if (this.current_dur < duration) { // 球员处于未力竭时
+                    if (this.v < vmax) {
+                        this.v += vmax / (acc_time * unit_time);
+                    } else {
+                        this.current_dur += 1 / unit_time;
+                        this.v = vmax;
+                    }
+                } else {
+                    this.v = vmax / 2; // 设力竭后速度稳定为极速 1/2
+                }
+                this.x += this.v * Math.cos(this.angle) / unit_time;
+                this.y += this.v * Math.sin(this.angle) / unit_time;
+                place_last = place_now;
+                place_now = Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y));
+            } else if (this.v >= 0 && stayInBound(this, ground)) { // 到达终点后还要跑一段
+                this.v -= 2 * vmax / (acc_time * unit_time); // 设减速度是加速度 2 倍
+                this.x += this.v * Math.cos(this.angle) / unit_time;
+                this.y += this.v * Math.sin(this.angle) / unit_time;
+            } else { // 也可用 Promise 处理，这个 else 代码块的内容作为 .then() 的内容。使用 Promise 注意要 var that = this;
+                this.v = 0;
+                clearInterval(this.timer);
+            }
+        }, interval);
+    }
+
+    kick(angle, v, ball, ground) {
+        var max_power_v = 5 + 45 * (this.power - 1) / 98,
+            sigma_base = 10 - 8 * (this.skill - 1) / 98; // 标准差的基数
+        var man_kick_angle = getBetweenAngle(angle, this.angle), // 球员角度与踢球角度的夹角
+            man_ball_angle = getBetweenAngle(this.angle, ball.angle); // 球员角度与球运动角度的夹角
+        var angle_offset = 0,
+            sigma_offset = 1,
+            max_kick_v = max_power_v;
+
+        if (this.v > 0) { // 人运动时，有射出角度、力度的影响
+            max_kick_v = max_power_v * (1.4 - man_kick_angle / Math.PI * 0.8);
+            sigma_offset = Math.sqrt(1 + man_kick_angle / Math.PI);
+        }
+        if (ball.v > 0) { // 球运动时，有射出角度的影响
+            angle_offset = Math.PI / 6 * -Math.abs(man_ball_angle - Math.PI / 2) / 3 * getGaussianRandom(0, 0.05 * sigma_base);
+        }
+
+        var actual_angle = getGaussianRandom(angle + angle_offset, Math.PI / 20 * sigma_base * sigma_offset), // 为了不让球员太菜，标准差设置得比较小
+            actual_v = getGaussianRandom(v, v / 30 * sigma_base);
+        actual_v = actual_v < 0 ? 0 : actual_v > max_kick_v ? max_kick_v : actual_v;
+
+        ball.move(actual_angle, actual_v, ground);
+    }
+}
+
+class Ball {
+    constructor() {
+        this.radius = 0.25;
+        this.x = 0;
+        this.y = 0;
+        this.angle = 0;
+        this.v = 0;
+        this.timer = null; // 为了可以随时停止运动，将计时器设为对象本身属性
+    }
+
+    move(angle, start_v, ground, interval = 10) {
+        this.v = start_v;
+        this.angle = angle;
+        var unit_time = 1000 / interval;
+        this.timer = setInterval(() => {
+            if (this.v > 0 && stayInBound(this, ground)) {
+                this.v += ground.ball_acc / unit_time;
+                this.x += this.v * Math.cos(this.angle) / unit_time;
+                this.y += this.v * Math.sin(this.angle) / unit_time;
+            } else {
+                this.v = 0;
+                clearInterval(this.timer);
+            }
+        }, interval);
+    }
+}
+
+
+class Ground {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this.ball_acc = -10; // 球在球场运动的阻力
+    }
+}
